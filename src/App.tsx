e9e47b60d489b3152f39f28d286877d444f4973e
Naziullah Shawn <mshawn192106@bscse.uiu.ac.bd>
2021-07-26 21:54:37 +0600

    added cart button

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
+import { Badge } from "@material-ui/core";
+import Drawer from "@material-ui/core/Drawer";
 import Grid from "@material-ui/core/Grid";
 import LinearProgress from "@material-ui/core/LinearProgress";
+import AddShoppingCartIcon from "@material-ui/icons/AddShoppingCart";
+import { useState } from "react";
 import { useQuery } from "react-query";
 // Stlyes
-import { Wrapper } from "./App.styles";
+import { StyledButton, Wrapper } from "./App.styles";
 // Components
 import Item from "./Item/Item";
 
@@ -21,13 +25,15 @@ const getProducts = async (): Promise<CartItemType[]> =>
   await (await fetch("https://fakestoreapi.com/products")).json();
 
 const App = () => {
+  const [cartOpen, setCartOpen] = useState(false);
+  const [cartItems, setCartItems] = useState([] as CartItemType[]);
   const { data, isLoading, error } = useQuery<CartItemType[]>(
     "products",
     getProducts
   );
   console.log(data);
 
-  const getTotalItems = () => null;
+  const getTotalItems = (items: CartItemType[]) => null;
 
   const handleAddToCart = (clickedItem: CartItemType) => null;
 
@@ -38,6 +44,14 @@ const App = () => {
 
   return (
     <Wrapper>
+      <Drawer anchor="right" open={cartOpen} onClose={() => setCartOpen(false)}>
+        Cart goes here
+      </Drawer>
+      <StyledButton onClick={() => setCartOpen(true)}>
+        <Badge badgeContent={getTotalItems(cartItems)} color="error">
+          <AddShoppingCartIcon />
+        </Badge>
+      </StyledButton>
       <Grid container spacing={3}>
         {data?.map((item) => (
           <Grid item key={item.id} xs={12} sm={4}>
